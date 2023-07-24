99d997883753d35b8db0019ef8728564b9cc8fb5
Immov <immanuel.giovano@gmail.com>
2023-07-24 10:45:34 +0700

    Route naming

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,17 @@
-<h1>This is index [Invoke]</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+
+<body>
+	<h1>This is index [Invoke]</h1>
+	<a href={{ route('blog.index') }}>Blog</a> <br>
+	<a href={{ route('blog.show', ['id' => 1]) }}>Blog-1</a>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+/*	| Web Routes
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider within a group which
+	| contains the "web" middleware group. Now create something great!
+	|
 */
 
-/*
-	HTTP Methods:
+/*	HTTP Methods:
 	GET - Request a resource
 	PUT - Update a resource (Modify every single values)
 	PATCH - Modify a resource (Onlymodifies the values that have been changed)
@@ -25,41 +20,67 @@ use Illuminate\Support\Facades\Route;
 */
 
 // GET
-Route::get('/blog', [PostController::class, 'index']);
-Route::get('/blog/{id?}/{name?}', [PostController::class, 'show'])
+Route::get('/blog', [PostController::class, 'index'])
+	->name('blog.index'); // a href ={{ route('blog.index') }}
+Route::get("/blog/{id}", [PostController::class, 'show'])
+	->name('blog.show'); // route('blog.show', ['id' => 1])
+
+
+// POST
+Route::get('/blog/create', [PostController::class, 'create'])
+	->name('blog.create');
+Route::post('/blog', [PostController::class, 'store'])
+	->name('blog.store');
+
+// PUT or PATCH
+Route::get('/blog/edit/{id}', [PostController::class, 'edit'])
+	->name('blog.edit');
+Route::patch('/blog/{id}', [PostController::class, 'update'])
+	->name('blog.update');
+
+// DELETE
+Route::delete('/blog/{id}', [PostController::class, 'destroy'])
+	->name('blog.destroy');
+
+
+
+
+// Route for invoke method (No need to put function name after HomeController::class) --> Good for Single function controller
+Route::get('/', HomeController::class);
+
+
+
+
+
+// ==========NOTES==========
+
+/*	Route Resource
+	// Includes all the methods(index, show, create, store, edit, update, destroy)
+	// Also have a premade names like blog.index, blog.destroy, etc
+	// Route::resource('blog', PostController::class);
+*/
+
+// Return view
+// Route::view('/blog', 'blog.index', ['name' => 'Immov']);
+
+/*	REGEX for parameter
+	Route::get('/blog/{id?}/{name?}', [PostController::class, 'show'])
 	->where([
 		'id' => '[0-9]+', // or use ->whereNumber('id')
 		'name' => '[A-Za-z]+' // or use ->whereAlpha('name')
-	]);
-/* // OR
-Route::get('/blog/{id}/{name}', [PostController::class, 'index'])
+	]); */
+
+/*	OR
+	Route::get('/blog/{id}/{name}', [PostController::class, 'index'])
 	->whereNumber('id')
 	->whereAlpha('name');
  */
 
 
-// POST
-Route::get('/blog/create', [PostController::class, 'create']);
-Route::post('/blog', [PostController::class, 'store']);
-
-// PUT or PATCH
-Route::get('/blog/edit/{id}', [PostController::class, 'edit']);
-Route::patch('/blog/{id}', [PostController::class, 'update']);
-
-// DELETE
-Route::delete('/blog/{id}', [PostController::class, 'destroy']);
-
 // Multiple HTTP verbs
+
 // Route any methods inside [], in this case GET and POST, to run index on PostController
-Route::match(['GET', 'POST'], '/blog', [PostController::class, 'index']);
+// Route::match(['GET', 'POST'], '/blog', [PostController::class, 'index']);
+
 // Route ANY methods of /blog, to run index on PostController
-Route::any('/blog', [PostController::class, 'index']);
-
-// Resource Controller READ(https://laravel.com/docs/9.x/controllers#resource-controllers)
-// Route::resource('blog', PostController::class);
-
-// Route for invoke method (No need to put function name after HomeController::class) --> Good for Single function controller
-Route::get('/', HomeController::class);
-
-// Return view
-// Route::view('/blog', 'blog.index', ['name' => 'Immov']);
+// Route::any('/blog', [PostController::class, 'index']);
