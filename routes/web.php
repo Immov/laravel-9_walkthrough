4f0a199efb5fb7184e62c15affb8756116863394
Immov <immanuel.giovano@gmail.com>
2023-07-27 15:30:07 +0700

    Fix /blog/create with whereNumber restriction

--- routes/web.php
+++ routes/web.php
@@ -29,18 +29,18 @@ Route::prefix('/blog')
 	->group(function () {
 		// GET
 		Route::get('/', [PostController::class, 'index'])->name('blog.index'); // a href ={{ route('blog.index') }}
-		Route::get("/{id}", [PostController::class, 'show'])->name('blog.show'); // route('blog.show', ['id' => 1])
+		Route::get("/{id}", [PostController::class, 'show'])->name('blog.show')->whereNumber('id'); // route('blog.show', ['id' => 1])
 
 		// POST
 		Route::get('/create', [PostController::class, 'create'])->name('blog.create'); // <-- This should be a POST request
 		Route::post('/create', [PostController::class, 'store'])->name('blog.store'); // <-- Use POST for creating a new blog post
 
 		// PUT or PATCH
-		Route::get('/edit/{id}', [PostController::class, 'edit'])->name('blog.edit');
-		Route::patch('/{id}', [PostController::class, 'update'])->name('blog.update');
+		Route::get('/edit/{id}', [PostController::class, 'edit'])->name('blog.edit')->whereNumber('id');
+		Route::patch('/{id}', [PostController::class, 'update'])->name('blog.update')->whereNumber('id');
 
 		// DELETE
-		Route::delete('/{id}', [PostController::class, 'destroy'])->name('blog.destroy');
+		Route::delete('/{id}', [PostController::class, 'destroy'])->name('blog.destroy')->whereNumber('id');
 	});
 
 
